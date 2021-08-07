694e10780401580b163e56f481a369f1ef5125ac
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-08-07 12:41:19 +0600

    swagger->configure swagger in startup.cs and launchSettings.json

--- Portfolio.API/Startup.cs
+++ Portfolio.API/Startup.cs
@@ -26,11 +26,31 @@ namespace Portfolio.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            /*Register the Swagger generator-start*/
+            //services.AddSwaggerGen();
+            services.AddSwaggerGen(
+                (options)=> 
+                {
+                    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+                    options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v2" });
+                }
+                );
+            /*Register the Swagger generator-end*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            /*Enable middleware to serve generated Swagger as a JSON endpoint-start*/
+            app.UseSwagger();
+            /*Enable middleware to serve generated Swagger as a JSON endpoint-end*/
+            /*specify the Swagger JSON endpoint-start*/
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API v2");
+            });
+            /*specify the Swagger JSON endpoint-end*/
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
