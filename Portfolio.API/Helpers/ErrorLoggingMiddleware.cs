d930aef6532f7c80262c2b66c8133f2385e93b06
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-08-14 23:07:34 +0600

    Handle exceptions in the middle->added ErrorLoggingMiddleware class for the Extended methods

---
+++ Portfolio.API/Helpers/ErrorLoggingMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Portfolio.API.Helpers
+{
+    public class ErrorLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        //private readonly ILogger _logger;
+        public ErrorLoggingMiddleware(RequestDelegate next/*, ILoggerFactory loggerFactory*/)
+        {
+            _next = next;
+            //_logger = loggerFactory.CreateLogger<RequestResponseLoggingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                //_logger.LogError(exception, "Exception Caught in Exception Middleware");
+
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (exception)
+                {
+                    case AppException e:
+                        // custom application error
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    case KeyNotFoundException e:
+                        // not found error
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        // unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonConvert.SerializeObject(new { message = exception?.Message });
+                await response.WriteAsync(result);
+            }
+
+
+        }
+    }
+    public class AppException : Exception
+    {
+        public AppException() : base() { }
+
+        public AppException(string message) : base(message) { }
+
+        public AppException(string message, params object[] args)
+            : base(String.Format(System.Globalization.CultureInfo.CurrentCulture, message, args))
+        {
+        }
+    }
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorLoggingMiddleware>();
+        }
+    }
+}
