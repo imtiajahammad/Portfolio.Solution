095773db239ed28392f3bbb1dda59f173b53a8a4
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-08-14 13:00:00 +0600

    Validate Request Header->added class to use as middleware to the HTTP request pipeline #entensionMethods

---
+++ Portfolio.API/Helpers/ValidateHeaderMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.API.Helpers
+{
+    public class ValidateHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ValidateHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+            if (!httpContext.Request.Path.Value.Contains("Login") && !httpContext.Request.Path.Value.Contains("Swagger"))
+            {
+                string authorization = httpContext.Request.Headers["Authorization"];
+                if (authorization == null)
+                {
+                    httpContext.Response.StatusCode = 401;
+                    await httpContext.Response.WriteAsync("Access denied Token Missing!");
+                    return;
+                }
+
+                string versionheader = httpContext.Request.Headers["x-frapper-api-version"];
+                if (versionheader == null)
+                {
+                    httpContext.Response.StatusCode = 401;
+                    await httpContext.Response.WriteAsync("Access denied Version Header Missing!");
+                    return;
+                }
+
+            }
+
+            await _next(httpContext);
+        }
+    }
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ValidateHeaderMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseValidateHeaderMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ValidateHeaderMiddleware>();
+        }
+    }
+}
