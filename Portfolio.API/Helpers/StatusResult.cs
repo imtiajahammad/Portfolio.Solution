511fcc0fa812870e2ed8a889f0b42b59e8e6c261
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-08-15 11:20:15 +0600

    Common Response-> dynamic type class for response Model

---
+++ Portfolio.API/Helpers/StatusResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.API.Helpers
+{
+    public class StatusResult<T>
+    {
+        public ResponseStatus Status { get; set; }
+        public string Message { get; set; }
+        public T Result { get; set; }
+        public StatusResult()
+        {
+            Status = ResponseStatus.Failed;
+            Message = String.Empty;
+        }
+    }
+    public enum ResponseStatus
+    {
+        Failed = 0,
+        Success = 1,
+        NotFound = 2,
+        LoginSuccess=3,
+        FetchSuccess=4
+    }
+}
