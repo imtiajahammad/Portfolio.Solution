3470e242df99fabc778194ed93f754da82dec889
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-08-27 16:27:22 +0600

    CRUD(MOVIES)->VIEWMODEL PROJECT

---
+++ Portfolio.ViewModel/Movies/Request/MoviesCreateRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Portfolio.ViewModel.Movies.Request
+{
+    public class MoviesCreateRequest
+    {
+        [Required(ErrorMessage = "Enter Name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Director")]
+        [Display(Name = "Director")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Enter Genre")]
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Enter Title")]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Enter ReleaseYear")]
+        [Display(Name = "ReleaseYear")]
+        public int ReleaseYear { get; set; }
+
+        [Required(ErrorMessage = "Enter Rating")]
+        [Display(Name = "Rating")]
+        public string Rating { get; set; }
+
+        [Required(ErrorMessage = "Enter MovieLength")]
+        [Display(Name = "MovieLength")]
+        public string MovieLength { get; set; }
+    }
+}
