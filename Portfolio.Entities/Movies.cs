dd3ba12c8e032082c91cb09c1f167fd2c0ee12ee
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-08-27 16:26:27 +0600

    CRUD(MOVIES)->Entities

---
+++ Portfolio.Entities/Movies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Portfolio.Entities
+{
+    [Table("Movies")]
+    public class Movies
+    {
+        [Key]
+        public int MoviesID { get; set; }
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Rating { get; set; }
+        public string MovieLength { get; set; }
+    }
+}
