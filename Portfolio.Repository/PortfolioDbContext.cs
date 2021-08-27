c02ac4867c19744b4b0b600f3751d5825cd20502
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-08-27 16:38:34 +0600

    CRUD(MOVIES)->PortfolioDbContext class added

---
+++ Portfolio.Repository/PortfolioDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Repository
+{
+    public class PortfolioDbContext:DbContext
+    {
+        public PortfolioDbContext(DbContextOptions<PortfolioDbContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Entities.UserMaster.UserMaster> UserMasters { get; set; }
+        //public DbSet<AssignedRoles> AssignedRoles { get; set; }
+        //public DbSet<UserTokens> UserTokens { get; set; }
+        //public DbSet<RoleMaster> RoleMasters { get; set; }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
